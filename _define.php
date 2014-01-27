5be748dbf029e84f155ab461fe577c3f110de2d1
brol <brol31@gmail.com>
2014-01-27 12:14:30 +0100

    ajout class post

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
 	/* Name */				  'Acronyms Manager',
 	/* Description*/		'Add, remove and modify acronyms for the wiki syntax',
 	/* Author */			  'Vincent Garnier, Pierre Van Glabeke, Bernard Le Roux',
-	/* Version */			  '1.6',
+	/* Version */			  '1.6.1',
 	/* Properties */
 	array(
 		'permissions' => 'usage,contentadmin',
